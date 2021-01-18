ab159906ebccaef791c5ab61d899a09f1a79aa26
gq_glb <gq_glb@qq.com>
2021-01-18 20:41:18 +0800

    相册分享系统

--- src/main/java/com/setup/mapper/AlbumMapper.java
+++ src/main/java/com/setup/mapper/AlbumMapper.java
@@ -4,13 +4,17 @@ import com.setup.entity.Album;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @Mapper
 public interface AlbumMapper {
      //创建相册
      public Album createAlbum();
-
-
+     //查找所有相册
+     public List<Album> queryAlbum();
+     //删除相册
+     public boolean delAlbum(int aid);
 
 
 
