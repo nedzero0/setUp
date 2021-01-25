db8ca46527b5bd0be1f18b7a9febe701bada49c4
gq_glb <gq_glb@qq.com>
2021-01-25 19:57:14 +0800

    相册分享系统

---
+++ src/main/java/com/setup/config/MyMvcConfig.java
@@ -0,0 +1,17 @@
+package com.setup.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+    //设置视图映射，添加重定向
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index.html").setViewName("index");
+        registry.addViewController("/login.html").setViewName("login");
+        registry.addViewController("/own/personal.html");
+    }
+}
--- src/main/java/com/setup/controller/UserController.java
+++ src/main/java/com/setup/controller/UserController.java
@@ -1,17 +1,52 @@
 package com.setup.controller;
 
-import org.springframework.context.annotation.Configuration;
+import com.setup.entity.User;
+import com.setup.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
-    //登录页面
-    @RequestMapping("/login")
+    @Autowired
+    private UserService userService;
+
+    //默认页面
+    @RequestMapping("/index")
     public String success(){
         System.out.println("1111111");
         return "index";
     }
+
+    //到登录页面
+    @RequestMapping("/getLogin")
+    public String getLogin(){
+        return "login";
+    }
+
+    //进行登录
+    @RequestMapping("/login")
+    public String login(String UserPhone, String Password, HttpSession session){
+        //System.out.println(UserPhone+""+Password);
+        System.out.println("登录中");
+        User user = userService.signIn(UserPhone, Password);
+        System.out.println(user);
+        if (user!=null){
+            session.setAttribute("user",user);
+            System.out.println(session.getAttribute("str"));
+            System.out.println("登录成功");
+            return "redirect:/own/personal.html";
+        }else {
+            System.out.println("登录失败");
+            return "redirect:/login.html";
+        }
+    }
+
+
 
 
 }
